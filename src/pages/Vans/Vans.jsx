5e4f01dfdaf051c910ae6ce308833166fbd9883d
Haleigh <haleighvalentine@gmail.com>
2024-06-06 19:12:55 +0200

    Filter the vans in VanLife

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -6,7 +6,6 @@ export default function Vans() {
 	const [vans, setVans] = React.useState([]);
 
 	const typeFilter = searchParams.get('type');
-	console.log(typeFilter);
 
 	React.useEffect(() => {
 		fetch('/api/vans')
@@ -14,7 +13,9 @@ export default function Vans() {
 			.then((data) => setVans(data.vans));
 	}, []);
 
-	const vanElements = vans.map((van) => (
+	const displayedVans = typeFilter ? vans.filter((van) => van.type === typeFilter) : vans;
+
+	const vanElements = displayedVans.map((van) => (
 		<div key={van.id} className="van-tile">
 			<Link to={`/vans/${van.id}`}>
 				<img src={van.imageUrl} />
