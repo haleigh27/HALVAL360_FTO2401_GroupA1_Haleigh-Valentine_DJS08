eb85b8a9d3703ee336eb8a9e393a32e0156575bb
Haleigh <haleighvalentine@gmail.com>
2024-06-06 18:51:14 +0200

    Add nested routes for Host Van Info, Pricing and Photos

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Income from './pages/Host/Income.jsx';
 import Reviews from './pages/Host/Reviews.jsx';
 import HostVans from './pages/Host/HostVans.jsx';
 import HostVanDetail from './pages/Host/HostVanDetail.jsx';
+import HostVanInfo from './pages/Host/HostVanInfo.jsx';
+import HostVanPricing from './pages/Host/HostVanPricing.jsx';
+import HostVanPhotos from './pages/Host/HostVanPhoto.jsx';
 import Layout from './components/Layout.jsx';
 import HostLayout from './components/HostLayout.jsx';
 
@@ -28,7 +31,11 @@ export default function App() {
 						<Route path="income" element={<Income />} />
 						<Route path="reviews" element={<Reviews />} />
 						<Route path="vans" element={<HostVans />} />
-						<Route path="vans/:id" element={<HostVanDetail />} />
+						<Route path="vans/:id" element={<HostVanDetail />}>
+							<Route index element={<HostVanInfo />} />
+							<Route path="pricing" element={<HostVanPricing />} />
+							<Route path="photos" element={<HostVanPhotos />} />
+						</Route>
 					</Route>
 				</Route>
 			</Routes>
