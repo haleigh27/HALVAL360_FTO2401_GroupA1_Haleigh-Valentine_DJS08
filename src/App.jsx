5fdbd14f6908ddbfed4ed69c0449a1ade67c9642
Haleigh <haleighvalentine@gmail.com>
2024-06-06 14:37:01 +0200

    Fix file paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Vans from './pages/Vans.jsx';
-import VanDetail from './pages/VanDetail.jsx';
+import Vans from './pages/Vans/Vans.jsx';
+import VanDetail from './pages/Vans/VanDetail.jsx';
 import Layout from './components/Layout.jsx';
 
 import './server.jsx';
