350f43a1032bfce55dbf4ffb6ea2c9314e87368d
Haleigh <haleighvalentine@gmail.com>
2024-06-06 14:22:46 +0200

    Fix Navbar with Layout Route and Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Vans from './pages/Vans.jsx';
 import VanDetail from './pages/VanDetail.jsx';
+import Layout from './components/Layout.jsx';
 
 import './server.jsx';
 
 export default function App() {
 	return (
 		<BrowserRouter>
-			<header>
-				<Link className="site-logo" to="/">
-					#VanLife
-				</Link>
-				<nav>
-					<Link to="/about">About</Link>
-					<Link to="/vans">Vans</Link>
-				</nav>
-			</header>
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/vans" element={<Vans />} />
-				<Route path="/vans/:id" element={<VanDetail />} />
+				<Route element={<Layout />}>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/vans" element={<Vans />} />
+					<Route path="/vans/:id" element={<VanDetail />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
