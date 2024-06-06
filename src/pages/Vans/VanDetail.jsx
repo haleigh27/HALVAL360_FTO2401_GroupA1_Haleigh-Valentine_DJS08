28da3913c55c6e6926a94b27b301d0f3e56d82af
Haleigh <haleighvalentine@gmail.com>
2024-06-06 19:41:34 +0200

    Link state and add useLocation

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams, useLocation } from 'react-router-dom';
 
 export default function VanDetail() {
 	const params = useParams();
+	const location = useLocation();
+	console.log(location);
+
 	const [van, setVan] = React.useState(null);
 
 	React.useEffect(() => {
@@ -11,8 +14,14 @@ export default function VanDetail() {
 			.then((data) => setVan(data.vans));
 	}, [params.id]);
 
+	const search = location.state?.search || '';
+
 	return (
 		<div className="van-detail-container">
+			<Link to={`..${search}`} relative="path" className="back-button">
+				&larr; <span>Back to all vans</span>
+			</Link>
+
 			{van ? (
 				<div className="van-detail">
 					<img src={van.imageUrl} />
