c39ef7c5c5e29c73b2fb257cb6246423adaeba7c
Haleigh <haleighvalentine@gmail.com>
2024-06-06 19:10:58 +0200

    Set up search params

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 export default function Vans() {
+	const [searchParams, setSearchParams] = useSearchParams();
 	const [vans, setVans] = React.useState([]);
+
+	const typeFilter = searchParams.get('type');
+	console.log(typeFilter);
+
 	React.useEffect(() => {
 		fetch('/api/vans')
 			.then((res) => res.json())
