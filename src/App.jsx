73608838d8137a34bf5e701bf62172ac49767159
Haleigh <haleighvalentine@gmail.com>
2024-06-06 16:21:58 +0200

    Add Host pages and HostLayout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Vans from './pages/Vans/Vans.jsx';
 import VanDetail from './pages/Vans/VanDetail.jsx';
+import Dashboard from './pages/Host/Dashboard.jsx';
+import Income from './pages/Host/Income.jsx';
+import Reviews from './pages/Host/Reviews.jsx';
 import Layout from './components/Layout.jsx';
+import HostLayout from './components/HostLayout.jsx';
 
 import './server.jsx';
 
@@ -11,11 +15,17 @@ export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route element={<Layout />}>
-					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/vans" element={<Vans />} />
-					<Route path="/vans/:id" element={<VanDetail />} />
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="about" element={<About />} />
+					<Route path="vans" element={<Vans />} />
+					<Route path="vans/:id" element={<VanDetail />} />
+
+					<Route path="host" element={<HostLayout />}>
+						<Route index element={<Dashboard />} />
+						<Route path="income" element={<Income />} />
+						<Route path="reviews" element={<Reviews />} />
+					</Route>
 				</Route>
 			</Routes>
 		</BrowserRouter>
