3be2f077ad4d9d8c2e5ee8066c8dc2e853d8e817
Haleigh <haleighvalentine@gmail.com>
2024-06-07 13:48:53 +0200

    Edit styling and add avatar icon img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,31 @@
 import { Link, NavLink } from 'react-router-dom';
+import avatarIcon from '../assets/images/avatar-icon.png';
 
 export default function Header() {
+	const activeStyles = {
+		fontWeight: 'bold',
+		textDecoration: 'underline',
+		color: '#161616',
+	};
+
 	return (
 		<header>
 			<Link className="site-logo" to="/">
 				#VanLife
 			</Link>
 			<nav>
-				<NavLink to="/host" className={({ isActive }) => (isActive ? 'active-link' : null)}>
+				<NavLink to="/host" style={({ isActive }) => (isActive ? activeStyles : null)}>
 					Host
 				</NavLink>
-				<NavLink to="/about" className={({ isActive }) => (isActive ? 'active-link' : null)}>
+				<NavLink to="/about" style={({ isActive }) => (isActive ? activeStyles : null)}>
 					About
 				</NavLink>
-				<NavLink to="/vans" className={({ isActive }) => (isActive ? 'active-link' : null)}>
+				<NavLink to="/vans" style={({ isActive }) => (isActive ? activeStyles : null)}>
 					Vans
 				</NavLink>
+				<Link to="login" className="login-link">
+					<img src={avatarIcon} className="login-icon" />
+				</Link>
 			</nav>
 		</header>
 	);
